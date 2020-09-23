4d376fdce3903a429e3acdc3df1c0ec597728747
Brayan lopez <brayan9105@hotmail.com>
2020-09-23 09:34:14 -0500

    Added faker to gemfile + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  nombre = Faker::Creature::Dog.name
+  creado_por = Faker::Name.unique.name
+  Checklist.create(nombre: nombre, creado_por: creado_por)
+end
